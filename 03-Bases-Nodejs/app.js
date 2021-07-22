d18d22d1eb4dbfc8334a8cd627e4c2fc7c8bdc7c
JMTE <JMTEdeveloper@outlook.es>
2021-07-22 17:12:19 +0200

    "Imprimir la salida en un archivo txt con la funcion writeFile"

--- 03-Bases-Nodejs/app.js
+++ 03-Bases-Nodejs/app.js
@@ -1,16 +1,32 @@
 //Imprimir la tabla del 5 en la consola
+
+const fs= require("fs");
+
+const base=3;
 console.clear();
 
 console.log("-----------------------------");
-console.log("        TABLA DEL 5           ");
+console.log(`        TABLA DEL ${base}           `);
 console.log("-----------------------------");
 
 
 
 
-const base=5;
+
+let salida="";
+
+
 for (let index = 1; index < 11; index++) {
     const element = index*base;
-    console.log(`${base} x ${index} = ${element}`);
+    salida += `${base} x ${index} = ${element}\n`;
     
 }
+
+console.log(salida);
+
+fs.writeFile(`tablal-${base}.txt`, salida, (err)=>{
+
+    if (err) throw err;
+
+    console.log("Archivo creado");
+})
