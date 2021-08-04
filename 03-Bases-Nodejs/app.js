4bcb625c9d8244e4734098e6e9e79ae000ecdac6
JMTE <JMTEdeveloper@outlook.es>
2021-08-04 18:07:01 +0200

    Se crea un archivo independiente para yargs y se exporta e importa respectivamente para que la app siga funcionando

--- 03-Bases-Nodejs/app.js
+++ 03-Bases-Nodejs/app.js
@@ -1,5 +1,5 @@
 //Imprimir la tabla del 5 en la consola
-
+const argv= require("./config/yargs");
 
 
 const { boolean } = require("yargs");
@@ -7,42 +7,12 @@ const {crearArchivo}= require("./helpers/multiplicar");
 // const base=3;
 
 
-const argv=require("yargs")
-.option("b", {
-
-alias:"base",
-type:"number",
-demandOption:true
-
-
-
-})
-
-
-
-.option("l", {
-
-    alias:"listar",
-    type: "boolean",
-    default:false
-})
-.check( (argv, options) => {
-
-    if (isNaN(argv.b)){
-        throw "La base tiene que ser un numero"
-    }
-    return true;
-})
-
-
-
-                    .argv;
 
 
 
 
 
-console.log(argv);
+
 
 //console.log(process.argv);
 
---
+++ 03-Bases-Nodejs/config/yargs.js
@@ -0,0 +1,39 @@
+const { describe } = require("yargs");
+
+const argv=require("yargs")
+.option("b", {
+
+alias:"base",
+type:"number",
+demandOption:true,
+describe:"Es la base de nuestra tabla de multiplicar"
+
+
+
+})
+
+
+
+.option("l", {
+
+    alias:"listar",
+    type: "boolean",
+    default:false,
+    describe:" Muestra los datos de la tabla en consola"
+})
+.check( (argv, options) => {
+
+    if (isNaN(argv.b)){
+        throw "La base tiene que ser un numero"
+    }
+    return true;
+})
+
+
+
+                    .argv;
+
+
+
+
+module.exports=argv;
